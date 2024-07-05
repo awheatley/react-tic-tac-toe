a94dbf2ae0f94fcb5384a2dec44b59300d58adb8
Adam Wheatley <wheatley.adam@me.com>
2024-07-05 11:34:15 -0500

    add editable Player component

---
+++ src/components/Player.jsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [playerName, setPlayerName] = useState(name);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleEditPlayer() {
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleSetPlayerName(event) {
+    setPlayerName(event.target.value);
+  }
+
+  let editedPlayerName = isEditing ? <input type="text" required value={playerName} onChange={handleSetPlayerName}/> : <span className="player-name">{playerName}</span>;
+
+  return (
+    <li>
+      <span className="player">
+        {editedPlayerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditPlayer}>{!isEditing ? 'Edit' : 'Save'}</button>
+    </li>
+  )
+}
